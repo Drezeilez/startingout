40026787a8f95f50252d02506b2947e9dbc4facb
Drezeilez <59574435+Drezeilez@users.noreply.github.com>
2021-08-14 01:18:20 +0530

    using positional argument

--- bash/fileExists.sh
+++ bash/fileExists.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "running $0 as $USER"
-if [[ -f "$file" ]]; then
-	echo file "$file" does exit;
+
+if [[ -f "$1" ]]; then
+	echo file "$1" does exit;
 else
-	echo file "$file" not found;
+	echo file "$1" not found;
 fi
